5ef793d0f0c0fb2f9db574dab1d67266ce56d48e
Anyella Perez <av.perezb@isc334.uniandes.edu.co>
2020-02-29 08:38:44 -0500

    create repo

---
+++ bbc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bbc
+//
+//  Created by ANYELLA VALERIA PEREZ BUENDIA on 29/02/20.
+//  Copyright © 2020 ANYELLA VALERIA PEREZ BUENDIA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
