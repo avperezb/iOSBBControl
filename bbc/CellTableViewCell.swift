40e5a9a7fd689c09828185c3199839509ec24774
Martin Galvan <md.galvan@uniandes.edu.co>
2020-03-01 02:20:18 -0500

    create repo

---
+++ bbc/CellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CellTableViewCell.swift
+//  bbc
+//
+//  Created by Martin David Galvan Castro on 29/02/20.
+//  Copyright © 2020 ANYELLA VALERIA PEREZ BUENDIA. All rights reserved.
+//
+
+import UIKit
+
+class CellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ bbc/TableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewController.swift
+//  bbc
+//
+//  Created by Martin David Galvan Castro on 29/02/20.
+//  Copyright © 2020 ANYELLA VALERIA PEREZ BUENDIA. All rights reserved.
+//
+
+import Foundation
